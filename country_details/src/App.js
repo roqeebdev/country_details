30155c32f188f27f3455af2d8d4abd4bece5b4c5
roqeebdev <akinyeleroqeeb@gmail.com>
2022-10-10 13:20:31 +0100

    new changes

---
+++ country_details/src/App.js
@@ -0,0 +1,14 @@
+
+import './NavBar';
+import Pagelayout from './Pagelayout';
+
+function App() {
+  return (
+    <div className="App">
+      <Pagelayout/>
+    </div>
+    
+  );
+}
+
+export default App;
